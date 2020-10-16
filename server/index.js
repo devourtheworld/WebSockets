20031f7700d0efd66691f18e9e07be70214bb5ca
zebestlove <zbyslav13@gmail.com>
2020-10-16 12:02:10 +0300

    web socket (server)

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({port: 8082});
+
+//start up WebSocket server (lines abowe)
+
+wss.on('connection', ws => {
+    console.log('New client connected!');
+
+    ws.on('message', data => {
+        console.log(`Client has sent us: ${data}`);
+
+        ws.send(data.toUpperCase());
+    });
+
+    ws.on('close', () =>{
+        console.log('Client has disconnected!');
+    });
+});
