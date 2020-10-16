288a89f5672e50e58425a09d4d3333369b7791ac
zebestlove <zbyslav13@gmail.com>
2020-10-16 12:03:34 +0300

    web sockets (server)

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const ws = new WebSocket('ws://localhost:8082');
+
+ws.addEventListener('open', () => {
+    console.log('We are connected!');
+    ws.send('Hey, how\'s it going?');
+});
+
+ws.addEventListener('message', ({data}) => {
+    console.log(data);
+});
